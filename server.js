493de0a5c2ff9b673ac0bf3a04611b01f656f5ba
muhammad9112003 <moayman9112003@gmail.com>
2025-11-25 16:45:08 +0200

    updated frontend fetch URLs

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500", // لو عايز تشغل من Live Server
+    origin: "*", 
     credentials: true,
   })
 );
